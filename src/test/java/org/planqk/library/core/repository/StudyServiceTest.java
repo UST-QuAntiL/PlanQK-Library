32a4fd35a2afbe3effe6a86c95aa5309f4ad71a8
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2022-03-23 15:57:06 +0100

    Add study service tests

---
+++ src/test/java/org/planqk/library/core/repository/StudyServiceTest.java
@@ -0,0 +1,66 @@
+package org.planqk.library.core.repository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.jabref.logic.importer.ParseException;
+import org.jabref.model.study.Study;
+import org.jabref.model.study.StudyDatabase;
+import org.jabref.model.study.StudyQuery;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StudyServiceTest {
+    @TempDir
+    Path workDir;
+    Path studiesDir;
+
+    @Test
+    public void createStudy() throws IOException {
+        StudyService service = StudyService.getInstance(workDir);
+        studiesDir = workDir.resolve("studies");
+        service.createStudy(getStudyDefinition("TestStudy"));
+        assertTrue(Files.isDirectory(studiesDir.resolve("TestStudy")));
+        assertTrue(Files.isRegularFile(studiesDir.resolve("TestStudy/study.yml")));
+    }
+
+    @Test
+    public void getStudyNames() throws IOException {
+        StudyService service = StudyService.getInstance(workDir);
+        studiesDir = workDir.resolve("studies");
+        service.createStudy(getStudyDefinition("TestStudy"));
+        service.createStudy(getStudyDefinition("TestStudy2"));
+
+        assertEquals(List.of("TestStudy", "TestStudy2"), service.getStudyNames());
+    }
+
+    @Test
+    public void deleteStudy() throws IOException {
+        StudyService service = StudyService.getInstance(workDir);
+        studiesDir = workDir.resolve("studies");
+        service.createStudy(getStudyDefinition("TestStudy"));
+        assertTrue(Files.isDirectory(studiesDir.resolve("TestStudy")));
+
+        service.deleteStudy("TestStudy");
+        assertFalse(Files.exists(studiesDir.resolve("TestStudy")));
+    }
+
+    public Study getStudyDefinition(String studyName) {
+        Study study = new Study();
+        study.setTitle(studyName);
+        study.setAuthors(List.of("A1", "A2"));
+        study.setQueries(List.of(new StudyQuery("testQuery1"), new StudyQuery("testQuery2")));
+        study.setDatabases(List.of(new StudyDatabase("arxiv", true), new StudyDatabase("ieeexplore", true)));
+        study.setResearchQuestions(List.of("Q1", "Q2"));
+        return study;
+    }
+}
